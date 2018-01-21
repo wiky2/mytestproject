259c2a34197a3459684a03d71a47452f77cadab7
wiky2 <jerry_136510@aliyun.com>
2018-01-21 12:28:28 +0800

    修改data3.txt中的邮箱

--- testforothers/testWordCount.py
+++ testforothers/testWordCount.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# coding: utf-8
 import requests
 from bs4 import BeautifulSoup
 import operator
@@ -14,5 +16,28 @@ def start(url):
         for each_word in words:
             print each_word
             word_list.append(each_word)
+    clean_up_list(word_list)
+def clean_up_list(word_list):
+    #clean data
+    clean_up_list=[]
+    for word in word_list:
+        symbols="!@#$%^&*()_<>?[]-=+{}|\""
+        for i in range(0,len(symbols)):
+            word=word.replace(symbols[i],"")
+        if len(word)>0:
+            print word
+            clean_up_list.append(word)
+    print clean_up_list
+    create_dictionary(clean_up_list)
+def create_dictionary(clean_up_list):
+    word_count={}
+    for word in clean_up_list:#判断是否在liszt中
+        if word in word_count:
+            word_count[word]+=1
+        else:
+            word_count[word]=1
+    for key ,value in sorted(word_count.items(),key=operator.itemgetter(1)):
+        print key,value
+
 start('https://www.washingtonpost.com/politics/how-the-trump-era-is-changing-the-federal-bureaucracy/2017/12/30/8d5149c6-daa7-11e7-b859-fb0995360725_story.html?utm_term=.48229016b77a')
 
